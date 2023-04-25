29374e8ab3ac7550ce4cbaf7ff4b2bc707343a3a
jarrett-pon-ziffdavis <80858626+jarrett-pon-ziffdavis@users.noreply.github.com>
2023-04-25 11:54:02 -0500

    feat: set compress to true

--- script/build.js
+++ script/build.js
@@ -1,43 +1,66 @@
-import {join} from 'path'
-import {terser} from 'rollup-plugin-terser'
-import size from 'rollup-plugin-size'
+import { join } from "path";
+import { terser } from "rollup-plugin-terser";
+import size from "rollup-plugin-size";
 
 const emitPackageScopeFile = (type) => {
-	return {
-		name: 'emit-package-scope-file',
-		generateBundle() {
-			this.emitFile({ type: 'asset', fileName: 'package.json', source: `{ "type": "${type}" }\n` });
-		}
-	};
-}
+  return {
+    name: "emit-package-scope-file",
+    generateBundle() {
+      this.emitFile({
+        type: "asset",
+        fileName: "package.json",
+        source: `{ "type": "${type}" }\n`,
+      });
+    },
+  };
+};
 
-const options = {mangle: true, compress: false, toplevel: true}
+const options = { mangle: true, compress: true, toplevel: true };
 const defaults = {
-	onwarn(warning, warn) {
-		switch (warning.code) {
-			case 'CIRCULAR_DEPENDENCY':
-				return
-			default:
-				warn(warning)
-		}
-	},
-	treeshake: {propertyReadSideEffects: false},
-	context: 'this'
-}
+  onwarn(warning, warn) {
+    switch (warning.code) {
+      case "CIRCULAR_DEPENDENCY":
+        return;
+      default:
+        warn(warning);
+    }
+  },
+  treeshake: { propertyReadSideEffects: false },
+  context: "this",
+};
 
-export default ({configSrc = './', configInput = join(configSrc, 'index.js')}) => {
-	return [
-		{
-			...defaults,
-			input: configInput,
-			output: [{file: join(configSrc, 'dist', 'umd', 'stylis.js'), format: 'umd', name: 'stylis', freeze: false, sourcemap: true}],
-			plugins: [emitPackageScopeFile('commonjs'), terser(options), size()]
-		},
-		{
-			...defaults,
-			input: configInput,
-			output: [{file: join(configSrc, 'dist', 'stylis.mjs'), format: 'esm', name: 'stylis', freeze: false, sourcemap: true}],
-			plugins: [terser(options), size()]
-		}
-	]
-}
+export default ({
+  configSrc = "./",
+  configInput = join(configSrc, "index.js"),
+}) => {
+  return [
+    {
+      ...defaults,
+      input: configInput,
+      output: [
+        {
+          file: join(configSrc, "dist", "umd", "stylis.js"),
+          format: "umd",
+          name: "stylis",
+          freeze: false,
+          sourcemap: true,
+        },
+      ],
+      plugins: [emitPackageScopeFile("commonjs"), terser(options), size()],
+    },
+    {
+      ...defaults,
+      input: configInput,
+      output: [
+        {
+          file: join(configSrc, "dist", "stylis.mjs"),
+          format: "esm",
+          name: "stylis",
+          freeze: false,
+          sourcemap: true,
+        },
+      ],
+      plugins: [terser(options), size()],
+    },
+  ];
+};
